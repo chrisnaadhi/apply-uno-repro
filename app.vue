00a88f856e2e8b9fd76cc0e3870958c2f7eec8e2
chrisnaadhi <chrisnaadhip@gmail.com>
2022-09-05 19:25:49 +0700

    first init

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <section class="one">One</section>
+    <section class="two">Two</section>
+    <section class="three">Three</section>
+  </div>
+</template>
+
+<style>
+.main {
+  @apply flex flex-row items-center justify-center;
+}
+section {
+  @apply h-screen w-full;
+}
+.one {
+  @apply bg-red-500 text-white md:bg-blue-500 xl:bg-orange-5;
+}
+
+.two {
+  @apply bg-gray-500 text-white md:bg-amber xl:bg-red-5;
+}
+
+.three {
+  @apply bg-purple-5 text-white md:bg-yellow-5 xl:bg-green-5;
+}
+</style>
